69a9c2665beabf203e4bbda4a040657ae8a11ea4
dmkim <56014886+kimdongmin-mydoctor@users.noreply.github.com>
2021-01-21 10:46:49 +0900

    기능: 헤더 표시 추가

---
+++ src/Component/Header/Header.tsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/styles';
+import { AppBar, Toolbar, Typography, IconButton, Badge } from '@material-ui/core';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
+
+type HeaderProps = {
+  classes: any,
+}
+
+
+const styles = {
+  grow: {
+    flexGrow: 1,
+    '&& a' : {
+      textDecoration: 'none',
+      color: '#ffffff',
+    }
+  },
+  toolbar: {
+    backgroundColor: '#000000',
+  },
+  title: {
+    display: 'block',
+    fontWeight: 600,
+    color: '#ffffff',
+    flexGrow: 1,
+  },
+}
+
+class Header extends Component<HeaderProps, {}> {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.grow}>
+        <AppBar position="fixed">
+          <Toolbar className={classes.toolbar} variant="dense">
+            <Typography className={classes.title} variant="h6" noWrap>
+              <a href='/'>CLASS 101</a>
+            </Typography>
+            <IconButton color="inherit">
+              <Badge badgeContent={0} color="secondary">
+                <ShoppingCartIcon />
+              </Badge>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </div>
+    )
+  }
+}
+
+export default withStyles(styles)(Header)
--- src/Component/View/ProductList.tsx
+++ src/Component/View/ProductList.tsx
@@ -5,6 +5,7 @@ import { numberWithCommas } from '../../Common/common';
 import { withStyles } from '@material-ui/styles';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Header from '../Header/Header'
 
 type ProductListProps = {
   classes: any
@@ -13,6 +14,7 @@ type ProductListProps = {
 const styles = {
   root: {
     maxWidth: 1920,
+    marginTop: 60,
   },
   cardRoot: {
     minWidth: 450,
@@ -43,42 +45,45 @@ class ProductList extends Component<ProductListProps, {}> {
   render() {
     const { classes } = this.props;
     return (
-      <Grid className={classes.root} container spacing={0} direction="column" justify="center" alignItems="center">
-        {
-          productItems.map((item: Product, index: number) => {
-            return (
-              <Grid item xs={12} sm={4} key={index}>
-                <Card className={classes.cardRoot}>
-                  <CardMedia
-                    className={classes.media}
-                    image={item.coverImage}
-                    title={item.title}
-                  />
-                  <CardContent>
-                    <Typography className={classes.title} variant="body2" color="textSecondary" component="p">
-                      {item.title}
-                    </Typography>
-                  </CardContent>
-                  <CardContent>
-                    <FavoriteIcon color="error" style={{ fontSize: 15 }} /><span className={classes.score}>{item.score}</span>
-                  </CardContent>
-                  <CardContent>
-                    <Grid component="div">
-                      {item?.availableCoupon !== false && <div className={classes.saleCoupon}><div>할인 쿠폰 적용 가능</div></div>}
-                      {`가격: ${numberWithCommas(item.price.toString())} 원`}
-                    </Grid>
-                  </CardContent>
-                  <CardActions disableSpacing>
-                    <IconButton aria-label="장바구니에 넣기">
-                      <AddShoppingCartIcon />
-                    </IconButton>
-                  </CardActions>
-                </Card>
-              </Grid>
-            )
-          })
-        }
-      </Grid>
+      <React.Fragment>
+        <Header />
+        <Grid className={classes.root} container spacing={0} direction="column" justify="center" alignItems="center">
+          {
+            productItems.map((item: Product, index: number) => {
+              return (
+                <Grid item xs={12} sm={4} key={index}>
+                  <Card className={classes.cardRoot}>
+                    <CardMedia
+                      className={classes.media}
+                      image={item.coverImage}
+                      title={item.title}
+                    />
+                    <CardContent>
+                      <Typography className={classes.title} variant="body2" color="textSecondary" component="p">
+                        {item.title}
+                      </Typography>
+                    </CardContent>
+                    <CardContent>
+                      <FavoriteIcon color="error" style={{ fontSize: 15 }} /><span className={classes.score}>{item.score}</span>
+                    </CardContent>
+                    <CardContent>
+                      <Grid component="div">
+                        {item?.availableCoupon !== false && <div className={classes.saleCoupon}><div>할인 쿠폰 적용 가능</div></div>}
+                        {`가격: ${numberWithCommas(item.price.toString())} 원`}
+                      </Grid>
+                    </CardContent>
+                    <CardActions disableSpacing>
+                      <IconButton aria-label="장바구니에 넣기">
+                        <AddShoppingCartIcon />
+                      </IconButton>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              )
+            })
+          }
+        </Grid>
+      </React.Fragment>
     );
   }
 }
